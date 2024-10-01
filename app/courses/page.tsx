f29b92f75de6599d5e6a1858b654ece3efb7080f
khmeu <alghost2004@gmail.com>
2024-10-01 10:11:19 +0300

    fix: added overflow handing for course card title

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -8,7 +8,6 @@ import React, { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { filterBy, filterBySearch } from "@/utils";
 
-
 const Courses = () => {
   const {
     loading,
@@ -26,20 +25,19 @@ const Courses = () => {
 
   useEffect(() => {
     if (courses.length > 0) {
-      const searchQuery = searchParams.get("q") || ""; 
-      const sortBy = searchParams.get("sortBy") || "name-asc"; 
+      const searchQuery = searchParams.get("q") || "";
+      const sortBy = searchParams.get("sortBy") || "name-asc";
 
-      const searchedData = filterBySearch(courses, searchQuery); 
+      const searchedData = filterBySearch(courses, searchQuery);
       const finalFilteredData = filterBy(searchedData, sortBy);
       setFilteredData(finalFilteredData);
     }
   }, [courses, searchParams]);
 
   return (
-    <div className="flex flex-1 flex-col w-full h-full my-10 px-8">
+    <div className="flex flex-1 flex-col w-full h-full mt-10 mb-20 px-8">
       <div className="mx-auto mb-10"></div>
       <div className="flex flex-col sm:flex-row items-center gap-3 w-[90dvw] p-6 bg-base-300 mx-auto mb-10 shadow-md rounded-xl">
-
         <SearchBar />
         <Filter />
       </div>
