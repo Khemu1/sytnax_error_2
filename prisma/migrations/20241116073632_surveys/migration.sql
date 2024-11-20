fd0751b211e3c514847cf0cc976f2aa73bb985ad
khmeu <alghost2004@gmail.com>
2024-11-20 15:32:01 +0200

    added migrations

--- prisma/migrations/20241116073632_surveys/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `groupName` on the `UserGroup` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "UserGroup" DROP COLUMN "groupName";
--- prisma/migrations/20241120133104_survey_builder/migration.sql
+++ prisma/migrations/20241120133104_survey_builder/migration.sql
@@ -23,24 +23,6 @@ CREATE TABLE "CorrectAnswer" (
 );
 
 -- CreateTable
-CREATE TABLE "Course" (
-    "id" SERIAL NOT NULL,
-    "userId" INTEGER NOT NULL,
-    "title" TEXT NOT NULL,
-    "instructorAndMentorInfo" TEXT NOT NULL,
-    "courseInfo" TEXT NOT NULL,
-    "price" INTEGER NOT NULL,
-    "totalSessions" INTEGER NOT NULL,
-    "totalSessionPerWeek" INTEGER NOT NULL,
-    "totalTasks" INTEGER NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3),
-    "deletedAt" TIMESTAMP(3),
-
-    CONSTRAINT "Course_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "Group" (
     "id" TEXT NOT NULL,
     "ownerId" INTEGER NOT NULL,
@@ -66,18 +48,11 @@ CREATE TABLE "Question" (
 );
 
 -- CreateTable
-CREATE TABLE "Role" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-
-    CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "Survey" (
     "id" TEXT NOT NULL,
     "workspaceId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "openInterval" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -97,60 +72,10 @@ CREATE TABLE "SurveySubmission" (
 );
 
 -- CreateTable
-CREATE TABLE "Token" (
-    "id" SERIAL NOT NULL,
-    "userId" INTEGER NOT NULL,
-    "token" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL,
-    "expiresAt" TIMESTAMP(3) NOT NULL,
-    "used" BOOLEAN NOT NULL DEFAULT false,
-
-    CONSTRAINT "Token_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "UrlData" (
-    "id" SERIAL NOT NULL,
-    "courseId" INTEGER NOT NULL,
-    "url" TEXT NOT NULL,
-    "imgurId" TEXT NOT NULL,
-    "deleteHash" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3),
-    "deletedAt" TIMESTAMP(3),
-
-    CONSTRAINT "UrlData_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
-    "username" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "passwordHash" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "deletedAt" TIMESTAMP(3),
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "UserRole" (
-    "id" SERIAL NOT NULL,
-    "userId" INTEGER NOT NULL,
-    "roleId" INTEGER NOT NULL,
-
-    CONSTRAINT "UserRole_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "UserGroup" (
     "id" TEXT NOT NULL,
-    "ownerId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
     "groupId" TEXT NOT NULL,
-    "groupName" TEXT NOT NULL,
 
     CONSTRAINT "UserGroup_pkey" PRIMARY KEY ("id")
 );
@@ -179,31 +104,13 @@ CREATE TABLE "WorkspaceGroup" (
 CREATE UNIQUE INDEX "Group_ownerId_key" ON "Group"("ownerId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Role_name_key" ON "Role"("name");
-
--- CreateIndex
 CREATE UNIQUE INDEX "Survey_workspaceId_key" ON "Survey"("workspaceId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "SurveySubmission_workspaceId_key" ON "SurveySubmission"("workspaceId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Token_token_key" ON "Token"("token");
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
--- CreateIndex
-CREATE UNIQUE INDEX "UserRole_userId_key" ON "UserRole"("userId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "UserRole_userId_roleId_key" ON "UserRole"("userId", "roleId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "UserGroup_groupId_ownerId_key" ON "UserGroup"("groupId", "ownerId");
+CREATE UNIQUE INDEX "UserGroup_groupId_userId_key" ON "UserGroup"("groupId", "userId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "WorkspaceGroup_groupId_workspaceId_key" ON "WorkspaceGroup"("groupId", "workspaceId");
@@ -218,9 +125,6 @@ ALTER TABLE "AnsweredQuestion" ADD CONSTRAINT "AnsweredQuestion_questionId_fkey"
 ALTER TABLE "CorrectAnswer" ADD CONSTRAINT "CorrectAnswer_questionId_fkey" FOREIGN KEY ("questionId") REFERENCES "Question"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Course" ADD CONSTRAINT "Course_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE "Group" ADD CONSTRAINT "Group_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
@@ -233,22 +137,10 @@ ALTER TABLE "Survey" ADD CONSTRAINT "Survey_workspaceId_fkey" FOREIGN KEY ("work
 ALTER TABLE "SurveySubmission" ADD CONSTRAINT "SurveySubmission_surveyId_fkey" FOREIGN KEY ("surveyId") REFERENCES "Survey"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Token" ADD CONSTRAINT "Token_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "UrlData" ADD CONSTRAINT "UrlData_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "UserRole" ADD CONSTRAINT "UserRole_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "UserRole" ADD CONSTRAINT "UserRole_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE "UserGroup" ADD CONSTRAINT "UserGroup_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "UserGroup" ADD CONSTRAINT "UserGroup_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "UserGroup" ADD CONSTRAINT "UserGroup_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Workspace" ADD CONSTRAINT "Workspace_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
